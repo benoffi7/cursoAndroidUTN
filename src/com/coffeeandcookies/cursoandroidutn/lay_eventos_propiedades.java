5cad6dd5a3bce906a60c985484323fd00f7a541b
Gonzalo Benoffi <benoffi7@gmail.com>
2013-08-28 16:09:03 -0300

    Cambios Clase #4
    Revison #3

--- src/com/coffeeandcookies/cursoandroidutn/lay_eventos_propiedades.java
+++ src/com/coffeeandcookies/cursoandroidutn/lay_eventos_propiedades.java
@@ -1,80 +1,139 @@
 package com.coffeeandcookies.cursoandroidutn;
 
-import java.util.ArrayList;
-
-import com.coffeeandcookies.cursoandroidutn.objetos.Casa;
-
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.RatingBar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class lay_eventos_propiedades extends Activity 
 {
-	private SeekBar barra_percha;
-	private TextView text_percha;
+	private SeekBar  barra;
+	private TextView text;
+	private Button 	 button;
+	private EditText edit;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		setContentView(R.layout.lay_seekbar);
 		levantarXML();
-		setupBarra();
-	
+		setupBarra();	
+		setupBoton();
+		setupEdit();
+		 //acceso a recurso del sistema
+        String nombre     = getResources().getString(R.string.app_name);
+        Drawable imagen   = getResources().getDrawable(R.drawable.ic_launcher);
+        String[] arreglo  = getResources().getStringArray(R.array.colores);
+        int idColor       = getResources().getColor(R.color.azul);      
+		
 		
 		super.onCreate(savedInstanceState);
 	}
 
+	private void setupEdit() 
+	{
+		edit.setOnFocusChangeListener(new OnFocusChangeListener() 
+		{			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) 
+			{
+				text.setText("onFocusChange: "+hasFocus);
+			}
+		});
+		edit.setOnKeyListener(new View.OnKeyListener() 
+		{			
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) 
+			{
+				text.setText("onKey: keyCode:"+keyCode+" keyEvente: "+event.getAction());
+				return false;
+			}
+		});
+		edit.addTextChangedListener(new TextWatcher() 
+		{
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) 
+			{
+				text.setText("onTextChanged: CharSequence:"+s);				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,	int after) 
+			{
+				// TODO Auto-generated method stub				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) 
+			{
+				// TODO Auto-generated method stub				
+			}
+		});
+	}
+
+	private void setupBoton() 
+	{
+		button.setOnClickListener(new View.OnClickListener() 
+		{			
+			@Override
+			public void onClick(View v) 
+			{
+				text.setText("Click en el boton");				
+			}
+		});
+		button.setOnLongClickListener(new View.OnLongClickListener() 
+		{			
+			@Override
+			public boolean onLongClick(View v) 
+			{
+				text.setText("Long-Click en el boton");
+				return false;
+			}
+		});
+	}
+
 	private void setupBarra() 
 	{
-		barra_percha.setProgress(50);
-		text_percha.setText("Valor: " + barra_percha.getProgress());
-		barra_percha.setMax(100);
-		barra_percha.setOnSeekBarChangeListener(new OnSeekBarChangeListener() 
+		barra.setProgress(50);
+		text.setText("Valor: " + barra.getProgress());
+		barra.setMax(100);
+		barra.setOnSeekBarChangeListener(new OnSeekBarChangeListener() 
 		{
-
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) 
 			{
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) 
 			{
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) 
 			{
-				text_percha.setText("Valor: " + progress);
+				text.setText("Valor: " + progress);
 			}
-		});
-		
+		});		
 	}
 
-	@Override
-	protected void onResume()
-	{
-		// TODO Auto-generated method stub
-		super.onResume();
-	}
 
-	@Override
-	protected void onPause() 
-	{
-		// TODO Auto-generated method stub
-		super.onPause();
-	}
 
 	private void levantarXML()
 	{
-		barra_percha = (SeekBar) findViewById(R.id.barra_percha);
-		text_percha = (TextView) findViewById(R.id.text_percha);
+		barra = (SeekBar) findViewById(R.id.barra);
+		text = (TextView) findViewById(R.id.text);
 	}
 }
--- src/com/coffeeandcookies/cursoandroidutn/lay_listas.java
+++ src/com/coffeeandcookies/cursoandroidutn/lay_listas.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.coffeeandcookies.cursoandroidutn.adaptadores.AdaptadorCasa;
 import com.coffeeandcookies.cursoandroidutn.objetos.Casa;
@@ -18,6 +23,57 @@ public class lay_listas extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
         
+        //COMBOS
+        
+        final String[] 	elementos = new String[]{"Elem1","Elem2","Elem3","Elem4","Elem5"};
+        final String[]  colores = getResources().getStringArray(R.array.colores);
+
+        ArrayAdapter<String> adaptador_elementos = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,elementos);
+        ArrayAdapter<String> adaptador_colores =  new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, colores);
+        
+        adaptador_elementos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adaptador_colores.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner comboElementos = (Spinner)findViewById(R.id.spinner_elementos);
+        Spinner comboColores = (Spinner)findViewById(R.id.spinner_colores);
+
+        comboElementos.setAdapter(adaptador_elementos);
+        comboColores.setAdapter(adaptador_colores);
+        
+        comboColores.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() 
+        {
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,int pos, long arg3)
+			{
+				Toast.makeText(getApplicationContext(), "Color seleccionado: "+colores[pos], Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+        comboElementos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,int pos, long arg3)
+			{
+				Toast.makeText(getApplicationContext(), "Elemento seleccionado: "+elementos[pos], Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) 
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        //LISTA
+        
         ArrayList<Casa> Casas = new ArrayList<Casa>();
         
         Casa oCasa = new Casa();
@@ -26,15 +82,10 @@ public class lay_listas extends Activity
         
         Casa oCasa1 = new Casa();
         oCasa1.setDireccion("Salta 4021");
-        Casas.add(oCasa1);                
-        AdaptadorCasa adaptadorCasa = new AdaptadorCasa(Casas, getApplicationContext());
-        ListView listView = (ListView)findViewById(R.id.lista);
-        listView.setAdapter(adaptadorCasa);
+        Casas.add(oCasa1);    
         
-        //acceso a recurso del sistema
-        String nombre     = getResources().getString(R.string.app_name);
-        Drawable imagen   = getResources().getDrawable(R.drawable.ic_launcher);
-        String[] arreglo  = getResources().getStringArray(R.array.colores);
-        int idColor       = getResources().getColor(R.color.azul);        
+        AdaptadorCasa adaptadorCasa = new AdaptadorCasa(Casas, getApplicationContext());
+        ListView listView = (ListView)findViewById(R.id.list_casas);
+        listView.setAdapter(adaptadorCasa);  
     }
 }
