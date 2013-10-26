4b896a2cd49cb278120bb7d97e8f23204eb40d3e
Gonzalo Benoffi <benoffi7@gmail.com>
2013-10-26 07:41:17 -0300

    Revision Clase #11

--- src/com/coffeeandcookies/cursoandroidutn/lay_camara.java
+++ src/com/coffeeandcookies/cursoandroidutn/lay_camara.java
@@ -138,40 +138,31 @@ public class lay_camara extends Activity
 		}
     }
 	
-	public void copyDatabaseToSdCard()
+	public void copiarDbASdCard()
 	{
-		Log.e("Databasehealper", "********************************");
 		try 
 		{
-			File f1 = new File("data/data/com.coffeeandcookies.ushuaiamovil/databases/comercios_V5");
+			File f1 = new File("data/data/com.coffeeandcookies.cursoandroidutn/databases/"+Configuracion.dbName);
 			if (f1.exists()) 
 			{
-				File f2 = new File(Environment.getExternalStorageDirectory().getAbsoluteFile()+ "/comercios_V5.db");
+				File f2 = new File(Environment.getExternalStorageDirectory().getAbsoluteFile()+ "/"+Configuracion.dbName+".db");
 				f2.createNewFile();
 				InputStream in = new FileInputStream(f1);
 				OutputStream out = new FileOutputStream(f2);
 				byte[] buf = new byte[1024];
 				int len;
-				while ((len = in.read(buf)) > 0) {
+				while ((len = in.read(buf)) > 0)
+				{
 					out.write(buf, 0, len);
 				}
 				in.close();
 				out.close();
 			}
 		} 
-		catch (FileNotFoundException ex) 
-		{
-			System.out.println(ex.getMessage() + " in the specified directory.");
-			System.exit(0);
+		catch (Exception ex) 
+		{			
 			ex.printStackTrace();
-		} 
-		catch (IOException e) 
-		{
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		Log.e("Databasehealper", "********************************");
+		} 		
 	}
 
 	private void guardarImagen(Bitmap photo)
